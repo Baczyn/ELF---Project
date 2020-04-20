6bc2b735a0e34e2a759aff7a1a6f18ab80b7661a
Baczyn <rac28@op.pl>
2020-04-20 21:01:00 +0200

    update skrypt

--- run_baczynski_mikolaj.sh
+++ run_baczynski_mikolaj.sh
@@ -3,11 +3,11 @@
 
 if [ $1 = "clone" ]; then
     git clone https://github.com/Baczyn/ELF---Project.git
+
 elif [ $1 = "run" ]; then
     sudo docker build -t projekt .
     sudo docker run -it projekt
-elif [ $1 = "solution" ]; then
-    echo $1
 elif [ $1 = "clean" ]; then
-   rm -rf ELF---Project
+
+   rm -rf Dockerfile Prezentacja_ELF.pdf Przyklady README.md Zadania
 fi
